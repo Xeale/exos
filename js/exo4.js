bd5af7c7489e0867c8b92be87f013e2a667700b6
Landreau-Jeffrey <landreau.jeffrey@gmail.com>
2020-10-20 09:39:57 +0200

    Exo 4

--- js/exo4.js
+++ js/exo4.js
@@ -7,3 +7,23 @@
  * Astuce : on peut avoir un objet de date en faisant : var now = new Date();
  * https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
  */
+
+
+
+function displayTime()
+{
+    let now = new Date();
+    let timeStr = "";
+    timeStr += now.getHours() + "h ";
+    timeStr += now.getMinutes() + "m ";
+    timeStr += now.getSeconds() + "s";
+    document.querySelector("#container").innerHTML = timeStr;
+}
+
+function init()
+{
+    setInterval(displayTime, 1000);
+}
+
+
+document.addEventListener("DOMContentLoaded" , init);
